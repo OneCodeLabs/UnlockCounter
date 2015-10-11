f170e93a2673e475812656dae984d053e4bcc889
goodengineer <gutierrez_nacho@hotmail.com>
2015-10-11 18:02:08 -0300

    service starts when boot is completed

---
+++ app/src/main/java/com/unlockchecker/unlockchecker/receiver/BootCompletedReceiver.java
@@ -0,0 +1,16 @@
+package com.unlockchecker.unlockchecker.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.unlockchecker.unlockchecker.service.UnlockCounterService;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent i = new Intent(context, UnlockCounterService.class);
+        context.startService(i);
+    }
+}
